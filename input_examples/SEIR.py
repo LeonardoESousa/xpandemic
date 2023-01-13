b2264a46bbc783c89c90ab3acaf2157e6b300b32
LeonardoESousa <leonardo.sousa137@gmail.com>
2023-01-13 04:38:18 +0100

    intervention class, new rates, Recovered class

--- input_examples/SEIR.py
+++ input_examples/SEIR.py
@@ -39,16 +39,24 @@ walk   = Walk(Rf=radii)
 incub = Incubation(time=3)
 
 ifr = {0:0.1,10:0.2,20:0.6,30:0.5,40:0.1,50:0.1}
-tau_inf = 3
+tau_inf = 2
 
 death = Death(time=tau_inf,ifr=ifr)
 cure  = Cure(time=tau_inf,ifr=ifr)
 
 ###PROCESSES#############################################################################
 
-processes = {'Susceptible':[walk], 'Exposed':[walk], 'Infectious':[walk]}
-monomolecular = {'Susceptible':[], 'Exposed':[incub],'Infectious':[death,cure]}
+processes = {'Susceptible':[walk], 'Exposed':[walk], 'Infectious':[walk], 'Recovered':[walk]}
+monomolecular = {'Susceptible':[], 'Exposed':[incub],'Infectious':[death,cure], 'Recovered':[ExpireImmunity(time=tau_inf)]}
 #########################################################################################
+r00 = 5
+radii = {0:r00,10:1.1*r00,20:1.2*r00,30:1.3*r00,40:1.2*r00,50:1*r00}
+## TRANSFER RATES
+walk2   = Walk(Rf=radii)
+death2  = Death(time=2,ifr={0:1,10:1,20:1,30:1,40:1,50:1})
+policy = {1:[{'Susceptible':[walk2], 'Exposed':[walk2], 'Infectious':[walk2]},{'Susceptible':[], 'Exposed':[incub],'Infectious':[death,cure]}]}
+#intervention = morphology.Intervention(policy)
+
 
 ###MORPHOLOGY############################################################################
 
@@ -68,7 +76,7 @@ lattice_func      = morphology.Map('mapa.txt',displacement)
 
 ##GENERATE PARTICLES#####################################################################
 method    = morphology.randomized
-exciton   = morphology.Create_Particles('Susceptible', 4, method, ages, mat=[1])
-exciton2  = morphology.Create_Particles('Infectious', 1, method, ages, mat=[1])
+#exciton   = morphology.Create_Particles('Susceptible', 4, method, ages, mat=[1])
+exciton2  = morphology.Create_Particles('Infectious', 4, method, ages, mat=[1])
 #########################################################################################
 
